1a1601aa20878cac7b96907d57426451a7ea149e
DuyLee157 <148028094+DuyLee157@users.noreply.github.com>
2023-11-14 12:40:14 +0700

    Add files via upload

---
+++ btvnss6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int i,n;
+    printf("bang cuu chuong\n");
+     for(int i=1;i<=10;i++){
+     for(int n=0;n<=10;n++){
+    int m=n*i;
+    printf("%d * %d = %d\n",m,n,i);
+}
+     }
+}
